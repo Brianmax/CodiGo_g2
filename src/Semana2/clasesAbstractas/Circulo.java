e5385bec63443d3979c874e03e24c8ee26090d68
Brianmax <george.maxi@ucsp.edu.pe>
2023-11-10 19:40:52 -0500

    update

--- src/Semana2/clasesAbstractas/Circulo.java
+++ src/Semana2/clasesAbstractas/Circulo.java
@@ -1,4 +1,19 @@
 package Semana2.clasesAbstractas;
 
-public class Circulo {
+public class Circulo extends Figura{
+    private float radio;
+    private final float PI = 3.1416f;
+    public Circulo(String color, boolean relleno, float radio){
+        super(color, relleno);
+        this.radio = radio;
+    }
+    @Override
+    public float area() {
+        return PI*radio*radio;
+    }
+
+    @Override
+    public float perimetro() {
+        return 2*PI*radio;
+    }
 }
--- src/Semana2/clasesAbstractas/Cuadrado.java
+++ src/Semana2/clasesAbstractas/Cuadrado.java
@@ -1,4 +1,18 @@
 package Semana2.clasesAbstractas;
 
-public class Cuadrado {
+public class Cuadrado extends Figura{
+    private float lado;
+    public Cuadrado(String color, boolean relleno, float lado){
+        super(color, relleno);
+        this.lado = lado;
+    }
+    @Override
+    public float area() {
+        return this.lado*this.lado;
+    }
+
+    @Override
+    public float perimetro() {
+        return lado*4;
+    }
 }
