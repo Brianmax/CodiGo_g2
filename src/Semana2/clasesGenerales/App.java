ed8c9a3e5e5af5c5e3140220952ade6267602fae
Brianmax <george.maxi@ucsp.edu.pe>
2023-11-10 21:10:58 -0500

    update

---
+++ src/Semana2/clasesGenerales/App.java
@@ -0,0 +1,10 @@
+package Semana2.clasesGenerales;
+
+import Semana2.estaticos.Usuario;
+
+public class App {
+    public static void main(String[] args) {
+        Punto2D<Integer> punto1 = new Punto2D<>(2,4);
+        Punto2D<Double> punto2 = new Punto2D<>(2.323, 23.4343);
+    }
+}
