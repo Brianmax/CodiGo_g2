3c8a368cf74aadb8981931459b8a0da9e5f5019c
Brianmax <george.maxi@ucsp.edu.pe>
2023-11-14 21:55:42 -0500

    first commit

---
+++ src/querys.sql
@@ -0,0 +1,32 @@
+create TABLE if not exists usuarios(
+    nombre varchar(30),
+    email varchar(30),
+    passworduser varchar(40),
+    fechaCreacion date default current_date
+    );
+create table if not exists categorias(
+                                         categoria serial primary key,
+                                         nombre varchar(20),
+    descripcion varchar(250)
+
+    );
+create table if not exists etiquetas(
+                                        post_id serial primary key,
+                                        nombre varchar(20),
+    descripcion varchar(250),
+    usuario integer,
+
+    foreign key (usuario) references usuario(usuario_id)
+    );
+
+create table if not exists posts(
+                                    post_id serial primary key,
+                                    titulo varchar(40),
+    contenido varchar(200),
+    fechaPublicacion date default current_date,
+    usuario integer,
+    categoria integer,
+
+    foreign key(usuario) references usuarios(usuario_id),
+    foreign key(categoria) references categorias(categoria_id)
+    );
