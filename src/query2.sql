6f23e98d14304adc4385b53493e88d56005a8810
Brianmax <george.maxi@ucsp.edu.pe>
2023-11-15 21:33:25 -0500

    update

--- src/query2.sql
+++ src/query2.sql
@@ -12,9 +12,9 @@ COMMIT;
 
 -- Ejecutar al inicio para reiniciar el contador de id
 ALTER SEQUENCE usuarios_usuario_id_seq RESTART WITH 1;
-ALTER SEQUENCE comentarios_comentario_id_seq RESTART WITH 1;
+ALTER SEQUENCE comentario_comentario_id_seq RESTART WITH 1;
 ALTER SEQUENCE etiquetas_etiqueta_id_seq RESTART WITH 1;
-ALTER SEQUENCE etiquetas_posts_etiquetas_posts_id_seq RESTART WITH 1;
+ALTER SEQUENCE etiquetas_posts_etiqueta_post_id_seq RESTART WITH 1;
 ALTER SEQUENCE posts_post_id_seq RESTART WITH 1;
 ALTER SEQUENCE categorias_categoria_id_seq RESTART WITH 1;
 
